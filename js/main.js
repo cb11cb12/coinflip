9df2fc2134fa2ba18fbc13fa141d6edf4e6ab5d6
eggsmayhem <flipmybit@protonmail.com>
2022-05-30 17:16:51 -0400

    halfway working coinflip animation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
+  // coin.style.animation = "none";
 
   const res = await fetch('/api')
   const data = await res.json()
 
   console.log(data);
   //suggestion: right before we change the text below, perhaps we could have the text flash red so the user can tell if it's changing from heads back to heads
+
+    // coin toss
+    const coin = document.getElementById("coin");
+    coin.style.animation = "none";
+    // coin toss
+     //coin toss
+      if (data.flip === 'Heads') {
+        coin.style.animation = "flip-heads 3s forwards"
+      }
+      else if (data.flip === 'Tails') {
+        coin.style.animation = "flip-tails 3s forwards"
+      }
+      //coin toss
+
+
   document.querySelector("#flipResponse").textContent = data.flip
 
 }
--- js/server.js
+++ js/server.js
@@ -4,6 +4,31 @@ const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 
+//function to serve coin image/ static files 
+
+// function serveStaticFile(res, path, contentType, responseCode) {
+//   if(!responseCode) responseCode = 200;
+
+//   // __dirname will resolve to the directory the executing script resides in.
+//   // So if your script resides in /home/sites/app.js, __dirname will resolve
+//   // to /home/sites.
+
+//   console.log(__dirname + path);
+
+//   fs.readFile(__dirname + path, function(err, data) {
+//       if(err) {
+//           res.writeHead(500, { 'Content-Type' : 'text/plain' });
+//           res.end('500 - Internal Error');
+//       } 
+//       else {
+//           res.writeHead( responseCode, { 'Content-Type' : contentType });
+//           res.end(data);
+//       }
+//   });
+// }
+
+//end static file server
+
 const server = http.createServer((req, res) => {
 
   const readWrite = (file, contentType) => {
@@ -23,6 +48,7 @@ const server = http.createServer((req, res) => {
     readWrite('index.html', 'text/html')
     
   }
+  //these elseifs can be removed
   else if (page == '/otherpage') {
     readWrite('otherpage.html', 'text/html')
     
@@ -31,10 +57,29 @@ const server = http.createServer((req, res) => {
     readWrite('otherotherpage.html', 'text/html')
     
   }
+  // above elseifs can be removed
+  //below is serving images of the coin
+  else if (page == '/img/cent-head.png') {
+    fs.readFile('img/cent-head.png', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }
+  // else if (page == '/img/cent-tail.png') {
+  //   fs.readFile('/img/cent-tail.png', function(err, data) {
+  //     res.write(data);
+  //     res.end();
+  //   });
+  // }
+
 
   //this connects to our fetch in main.js, thus the below is the coin flip object that is returned to main.js, and from there is put into the #flipResponse in our index.html DOM
   
   else if (page == '/api') {
+    //coin toss
+    // const coin = document.getElementById("coin");
+    // coin.style.animation = "none";
+    //coin toss
  
     let flipResult = Math.random() <= .5 ? 'Heads' : 'Tails' 
 
@@ -42,6 +87,20 @@ const server = http.createServer((req, res) => {
       const objToJson = {
         flip: flipResult
       }
+      //coin toss
+      // if (flipResult === 'Heads') {
+      //    fs.readFile('img/cent-head.png', function(err, data) {
+      //       res.write(data);
+      //       res.end();
+      //        });
+      // }
+      // else if (flipResult === 'Tails') {
+      //     fs.readFile('img/cent-head.png', function(err, data) {
+      //     res.write(data);
+      //     res.end();
+      //     });
+      // }
+      //coin toss
       res.end(JSON.stringify(objToJson));
     
     }else if (page == '/css/style.css'){
